a78a710c11cadfa6db56c51b0ded4adc4e89a56f
BenFestus <75408600+BenFestus@users.noreply.github.com>
2022-07-12 00:49:32 +0100

    Add files via upload

--- Guess-Game/Guess-Game.py
+++ Guess-Game/Guess-Game.py
@@ -5,12 +5,14 @@ def check_guess(guess, answer):
     guesses = 0
 
     #As long as user is still guessing and guesses is less than 2
-    while still_guessing and guesses < 2:
+    while still_guessing and guesses < 3:
         if guess.lower() == answer.lower():
             #Print correct answer when guess is correct
             print('Correct answer')
             #Add 1 to score
-            score = score + 1
+            score = score + 1 
+            #Add 1 to guesses
+            guesses = guesses + 1
             #Since user is correct, still_guessing is False
             still_guessing = False
             break
@@ -21,38 +23,37 @@ def check_guess(guess, answer):
             #Add 1 to the number of guesses 
             guesses = guesses +  1
 
-    if guesses == 2:
+    if guesses == 3:
         #Print the correct answer when user has guessed two more times and is wrong
         print('The correct answer is', answer)
 
-    
 
 score = 0
 
-print('Welcome to Guess Game')
+print('WELCOME TO GUESS GAME')
 
-guess = input('What is the capital of Nigeria? - ')
-check_guess(guess, 'Abuja')
+guess1 = input('What is the capital of Nigeria? - ')
+check_guess(guess1, 'Abuja')
 print()
 
-guess = input('What is the largest ocean mammal? - ')
-check_guess(guess, 'blue whale')
+guess2 = input('What is the largest ocean mammal? - ')
+check_guess(guess2, 'blue whale')
 print()
 
-guess = input('How many continents are there presently? - ')
-check_guess(guess, '7')
+guess3 = input('How many continents are there presently? - ')
+check_guess(guess3, '7')
 print()
 
-guess = input('What is the slowest land animal? - ')
-check_guess(guess, 'snail')
+guess4 = input('What is the slowest land animal? - ')
+check_guess(guess4, 'snail')
 print()
 
-guess = input('How many moons does Earth have? - ')
-check_guess(guess, '1')
+guess5 = input('How many moons does Earth have? - ')
+check_guess(guess5, '1')
 print()
 
-guess = input('What is the fastest land animal? - ')
-check_guess(guess, 'cheetah')
+guess6 = input('What is the fastest land animal? - ')
+check_guess(guess6, 'cheetah')
 print()
 
 print('Your score is', score)
